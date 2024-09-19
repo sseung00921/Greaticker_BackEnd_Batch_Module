0a49a389de23444a2757b93be843aa1d7a16a5fd
Sangseung Lee <sseung00921@naver.com>
2024-09-20 06:56:07 +0900

    batch test#2

--- src/main/java/com/example/greaticker/batch/config/SchedulingConfig.java
+++ src/main/java/com/example/greaticker/batch/config/SchedulingConfig.java
@@ -31,7 +31,7 @@ public class SchedulingConfig {
         this.job = job;
     }
 
-    @Scheduled(cron = "0 * * * * ?") // 매일 자정마다 실행
+    @Scheduled(cron = "0 0 0 * * ?") // 매일 자정마다 실행
     public void runUserWhoDoseNotGetStickerYesterdayRefreshProjectJob() {
         System.out.println("Batch is In Progress");
         LocalDate today = LocalDateTime.now().toLocalDate();
